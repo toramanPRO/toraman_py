a2b10a5db0d4b607974c3c557daa90aefdf2fd23
cagatay <cagataysengor@gmail.com>
2019-10-11 18:06:28 +0300

    version 0.1.0

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 import setuptools
 
-with open("README.md", "r") as input_file:
+from toraman import __version__
+
+with open('README.md', 'r') as input_file:
     long_description = input_file.read()
 
 setuptools.setup(
-    name="toraman",
-    version="0.0.2",
-    author="Çağatay Onur Şengör",
-    author_email="contact@csengor.com",
-    description="A computer assisted translation tool package",
+    name='toraman',
+    version=__version__,
+    author='Çağatay Onur Şengör',
+    author_email='contact@csengor.com',
+    description='A computer assisted translation tool package',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/csengor/toraman-py",
+    long_description_content_type='text/markdown',
+    url='https://github.com/csengor/toraman-py',
     packages=setuptools.find_packages(),
     install_requires=[
-        "lxml",
-        "regex"
+        'lxml',
+        'python-levenshtein',
+        'regex'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
