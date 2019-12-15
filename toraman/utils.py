f5fceae2489cd7f106c998087c483f032b5d4c8a
cagatay <cagataysengor@gmail.com>
2019-12-15 19:01:53 +0300

    html_to_segment and segment_to_html moved over from toraman_webapp

---
+++ toraman/utils.py
@@ -0,0 +1,50 @@
+import re
+
+from html import escape
+from lxml import etree
+
+def html_to_segment(source_or_target_segment, segment_designation, nsmap={'toraman': 'https://cat.toraman.pro'}):
+
+    segment = re.findall(r'<tag[\s\S]+?class="([\s\S]+?)">([\s\S]+?)</tag>|([^<^>]+)',
+                        source_or_target_segment)
+
+    segment_xml = etree.Element('{{{0}}}{1}'.format(nsmap['toraman'], segment_designation),
+                                nsmap=nsmap)
+    for element in segment:
+        if element[0]:
+            tag = element[0].split()
+            tag.insert(0, element[1][len(tag[0]):])
+            segment_xml.append(etree.Element('{{{0}}}{1}'.format(nsmap['toraman'], tag[1])))
+            segment_xml[-1].attrib['no'] = tag[0]
+            if len(tag) > 2:
+                segment_xml[-1].attrib['type'] = tag[2]
+        elif element[2]:
+            segment_xml.append(etree.Element('{{{0}}}text'.format(nsmap['toraman'])))
+            segment_xml[-1].text = element[2]
+
+    return segment_xml
+
+def segment_to_html(source_or_target_segment):
+    segment_html = ''
+    for sub_elem in source_or_target_segment:
+        if sub_elem.tag.endswith('}text'):
+            segment_html += escape(sub_elem.text)
+        else:
+            tag = etree.Element('tag')
+            tag.attrib['contenteditable'] = 'false'
+            tag.attrib['class'] = sub_elem.tag.split('}')[-1]
+            tag.text = tag.attrib['class']
+            if 'type' in sub_elem.attrib:
+                if sub_elem.attrib['type'] == 'beginning' or sub_elem.attrib['type'] == 'end':
+                    tag.attrib['class'] += ' ' + sub_elem.attrib['type']
+                else:
+                    tag.attrib['class'] += ' ' + 'standalone'
+            else:
+                tag.attrib['class'] += ' ' + 'standalone'
+
+            if 'no' in sub_elem.attrib:
+                tag.text += sub_elem.attrib['no']
+
+            segment_html += etree.tostring(tag).decode()
+
+    return segment_html
