872df5663d74c342427726bd334130da9cfcfdcb
cagatay <cagataysengor@gmail.com>
2019-12-25 23:09:05 +0300

    added from .utils import nsmap for backwards compatibility

--- toraman/__init__.py
+++ toraman/__init__.py
@@ -1,4 +1,5 @@
 from .bilingualfile import BilingualFile
 from .sourcefile import SourceFile
 from .translationmemory import TranslationMemory
+from .utils import nsmap
 from .version import __version__
